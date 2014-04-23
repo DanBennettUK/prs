8b3db7044d1d04a7ec62887c2d3f460152e1a805
Dan Bennett <dan@dan-bennett.me>
2014-04-23 13:28:04 +0100

    config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ *  Created on : 26-Oct-2013, 23:37:18
+ *  Author     : Dan Bennett
+ *  Website    : http://dan-bennett.me
+ */
+
+
+//Fill in the correct information:
+
+//Database Host (e.g. localhost)
+$dbhost = '';
+//Database Username
+$dbuser = '';
+//Database Password
+$dbpass = '';
+//Database Name
+$db = '';
